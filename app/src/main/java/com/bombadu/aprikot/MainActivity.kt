2bb785ed2a38ebec7b94f35bda0a388bc9d17ea6
michael <maydev99@gmail.com>
2019-10-12 16:37:18 -0400

    Added Save List to persistent memory

--- app/src/main/java/com/bombadu/aprikot/MainActivity.kt
+++ app/src/main/java/com/bombadu/aprikot/MainActivity.kt
@@ -1,16 +1,20 @@
 package com.bombadu.aprikot
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
+import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.*
 import androidx.appcompat.app.AlertDialog
-import androidx.core.view.get
-import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.android.synthetic.main.activity_main.view.*
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
+    var list = mutableListOf<String>()
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,10 +25,14 @@ class MainActivity : AppCompatActivity() {
         var textView = findViewById<TextView>(R.id.textView)
         var saveToListButton = findViewById<Button>(R.id.saveButton)
         var listView = findViewById<ListView>(R.id.list_view)
-        var num = 0
-        val list = mutableListOf<Int>()
+        //var hashSet = HashSet<String>()
 
-        var adapter = ArrayAdapter<Int>(this, android.R.layout.simple_list_item_1, list)
+        var num = 0
+
+        loadPrefs()
+
+
+        var adapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, list)
         listView.adapter = adapter
 
 
@@ -45,15 +53,54 @@ class MainActivity : AppCompatActivity() {
         }
 
         saveToListButton.setOnClickListener() {
-            list.add(num)
+            list.add(num.toString())
             adapter.notifyDataSetChanged()
+            saveToPrefs()
 
         }
 
         listView.onItemClickListener = AdapterView.OnItemClickListener { parent, view, position, id ->
             var listText = listView.getItemAtPosition(position)
+            var myItem = listText.toString()
             Toast.makeText(this, "Text: $listText", Toast.LENGTH_SHORT).show()
+            val intent = Intent(this, SecondActivity::class.java)
+            intent.putExtra("number", myItem)
+            startActivity(intent)
+
+
         }
+
+       listView.onItemLongClickListener = AdapterView.OnItemLongClickListener { parent, view, position, id ->
+           list.removeAt(position)
+           saveToPrefs()
+           adapter.notifyDataSetChanged()
+           true
+       }
+    }
+
+    private fun loadPrefs() {
+        var set :HashSet<String>
+        try {
+            val sharedPrefs: SharedPreferences = getSharedPreferences("prefs_key", Context.MODE_PRIVATE)
+            set = sharedPrefs.getStringSet("prefs_key", null) as HashSet<String>
+            list = set.toMutableList()
+            list.sortBy { it.toBigInteger() }
+        } catch (e: Exception){
+            e.printStackTrace()
+
+        }
+
+
+    }
+
+    private fun saveToPrefs() {
+        list.sortBy { it.toBigInteger() }
+        val set: HashSet<String> = HashSet(list)
+
+        val sharedPrefs: SharedPreferences = getSharedPreferences("prefs_key", Context.MODE_PRIVATE)
+        val editor: SharedPreferences.Editor = sharedPrefs.edit()
+        editor.putStringSet("prefs_key", set)
+        editor.commit()
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
--- app/src/main/java/com/bombadu/aprikot/SecondActivity.kt
+++ app/src/main/java/com/bombadu/aprikot/SecondActivity.kt
@@ -1,11 +1,22 @@
 package com.bombadu.aprikot
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.second_acitvity.*
 
 class SecondActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.second_acitvity)
+
+        val myItem = intent.getStringExtra("number")
+        val bigTextView = findViewById<TextView>(R.id.textView2)
+
+        bigTextView.text = myItem
+
+
+
 
     }
 }
