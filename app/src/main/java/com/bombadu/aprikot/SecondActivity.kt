27f805c59d181b9b1a004af53df67cfdefbbb424
michael <maydev99@gmail.com>
2019-10-14 19:20:38 -0400

    Added Array / For loop code on second screen

--- app/src/main/java/com/bombadu/aprikot/SecondActivity.kt
+++ app/src/main/java/com/bombadu/aprikot/SecondActivity.kt
@@ -1,7 +1,9 @@
 package com.bombadu.aprikot
 
 import android.os.Bundle
+import android.util.Log
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.second_acitvity.*
 
@@ -12,8 +14,34 @@ class SecondActivity : AppCompatActivity() {
 
         val myItem = intent.getStringExtra("number")
         val bigTextView = findViewById<TextView>(R.id.textView2)
+        val wordsTextView = findViewById<TextView>(R.id.arrayWordsTextView)
 
         bigTextView.text = myItem
+
+        var wordList = mutableListOf<String>()
+        wordList.add("Mike")
+        wordList.add("Cate")
+        wordList.add("Bob")
+        wordList.add("Mary")
+        wordList.add("Steve")
+        wordList.add("Gwen")
+        wordList.add("Hello")
+        wordList.add("Wow!")
+        wordList.add("Kotlin")
+
+        wordList.sort()
+
+
+        for (word in wordList) println(word)
+
+        /*for (word in wordList) {
+            println(word)
+
+        }*/
+
+        println(wordList.joinToString())
+
+
 
 
 
